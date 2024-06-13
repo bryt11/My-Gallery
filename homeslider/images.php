8ed1219477702844977866489449a0e08832319c
bryt11 <brightudodigwe@gmail.com>
2024-06-13 13:45:05 +0200

    Update images.php

--- homeslider/images.php
+++ homeslider/images.php
@@ -1,7 +1,6 @@
 <?php
-$images = glob("../images/*.jpg"); // Fetch all .jpg images from the 'images' directory
+$images = glob("../images/*.jpg");
 
 foreach ($images as $image) {
     echo "<img src='$image' alt='Slider Image'>";
 }
-// // echo "<img src='$images' >";
